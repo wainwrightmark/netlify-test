6e925874a88fec160619bef15402356f212d1da9
Mark Wainwright <wainwrightml@gmail.com>
2023-01-14 10:41:50 +0000

    Added stuff

---
+++ netlify/edge-functions/og-param-proxy.js
@@ -0,0 +1,15 @@
+import { Context } from "https://edge.netlify.com";
+
+
+export default async (request, context) => {
+    const url = new URL(request.url)
+    
+    // Get the page content.
+    const response = await context.next()
+    const page = await response.text()
+    
+    const search = 'test site'
+    const replace = `${url.searchParams.toString()}`
+    
+    return new Response(page.replaceAll(search, replace), response);
+}
