5251e092a02bdd5609b8fdee0b5245b22bb988ee
Mark Wainwright <wainwrightml@gmail.com>
2023-01-14 11:07:45 +0000

    added decode uri

--- netlify/edge-functions/og-param-proxy.js
+++ netlify/edge-functions/og-param-proxy.js
@@ -4,9 +4,11 @@ export default async (request, context) => {
     // Get the page content.
     const response = await context.next()
     const page = await response.text()
+	
+	const id = atob(decodeURIComponent(url.searchParams.toString()));
     
     const search = 'Logo/full+logo+small.jpg'
-    const replace = `Upscaled+Images/${atob(url.searchParams.toString())}.jpg`
+    const replace = `Upscaled+Images/${id}.jpg`
     
     return new Response(page.replaceAll(search, replace), response);
 }
